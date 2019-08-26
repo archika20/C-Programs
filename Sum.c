291b6ee119c2c4d814cf97d688f2604d76668866
Archika Vyas <53368347+archika20@users.noreply.github.com>
2019-08-26 18:00:40 +0530

    Create Sum.c

---
+++ Sum.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+  int arr[50];
+  int start,end;
+  int i,sum=0,n;
+  
+  printf("Enter the size of array :"); 
+  scanf("%d",&n);
+  
+  printf("enter the elements of array :\n");
+  for(i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+    
+  printf("enter your starting and ending index for there addition :\n");
+  scanf("%d %d",&start,&end);
+  
+  for(i=start;i<end+1;i++)
+  {
+    sum=sum+arr[i];
+  }
+  
+  printf("Sum is %d \n",sum);
+    
+}
